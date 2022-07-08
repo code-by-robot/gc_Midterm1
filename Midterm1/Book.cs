11eb588801c0aaad13ff2611e6506e5629887c9c
robot <rahaupt0@gmail.com>
2022-07-08 13:21:22 -0400

    added Book class

---
+++ Midterm1/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm1
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Status { get; set; }
+
+        //Constructor
+        public Book(string _title, string _author, string _status)
+        {
+            Title = _title;
+            Author = _author;
+            Status = _status;
+        }
+    }
+}

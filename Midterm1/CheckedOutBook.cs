3a4ec654a246fc7a292a236e93fec6d880425187
Connor <Woodconnor13@gmail.com>
2022-07-08 13:59:25 -0400

    Added 2 weeks to checkoutdate

--- Midterm1/CheckedOutBook.cs
+++ Midterm1/CheckedOutBook.cs
@@ -6,27 +6,20 @@ using System.Threading.Tasks;
 
 namespace Midterm1
 {
-    internal class Book
+    internal class CheckedOutBook : Book
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Status { get; set; }
-        public DateTime CheckOutDate = DateTime.Now;
-
-        //Constructor
-        public Book(string _title, string _author, string _status, DateTime _checkoutdate)
+        //properties
+        public DateTime CheckOutDate { get; set; }
+        //constructor
+        public CheckedOutBook(string _title, string _author, string _status, DateTime _checkoutdate) : base(_title, _author, _status, _checkoutdate)
         {
-            Title = _title;
-            Author = _author;
-            Status = _status;
             CheckOutDate = _checkoutdate;
         }
         //methods
         //ReturnDate takes in a DateTime x and adds 14 days.  This then returns the due date in a string
         public static DateTime ReturnDate(DateTime x)
         {
-            double test = 14;
-            x.AddDays(test);
+            x.AddDays(14);
             return x;
         }
         //string to datetime converter
@@ -35,5 +28,6 @@ namespace Midterm1
             DateTime checkOutDate = DateTime.Parse(x);
             return checkOutDate;
         }
+
     }
 }
--- Midterm1/Program.cs
+++ Midterm1/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using Midterm1;
+
+List<Book> AllBooks = new List<Book>()
+{
+    new Book("Ready Player One", "Ernest Cline", "On Shelf"),
+    new Book("1984", "George Orwell", "On Shelf"),
+    new Book("Starship Troopers", "Robert A. Heinlein", "On Shelf"),
+    new Book("2001: A Space Odyssey", "Arthur C. Clarke", "On Shelf"),
+    new Book("Twilight", "Stephenie Meyer", "On Shelf"),
+    new Book("The Hunger Games", "Suzanne Collins", "On Shelf"),
+    new Book("Astrophysics for People in a Hurry", "Neil deGrasse Tyson", "On Shelf"),
+    new Book("Scott Pilgrim Vol. 1-6", "Bryan Lee O'Malley", "On Shelf"),
+    new Book("One Piece Vol. 1-102", "Eiichiro Oda", "On Shelf"),
+    new Book("The Hobbit", "J.R.R. Tolkien", "On Shelf"),
+    new Book("Ender's Game", "Orson Scott Card", "On Shelf"),
+    new Book("The Great Gatsby", "F. Scott Fitzgerald", "On Shelf"),
+};
+
+DateTime testDate = DateTime.Now;
+Console.WriteLine(testDate);
+//DateTime returnDate = Book.ReturnDate(testDate);
+DateTime returnDate = testDate.AddDays(14);
+Console.WriteLine(returnDate);
+
+//DateTime x = DateTime.Now;
+//Console.WriteLine(x);
+//CheckedOutBook c = new CheckedOutBook("test", "tester", "available", )

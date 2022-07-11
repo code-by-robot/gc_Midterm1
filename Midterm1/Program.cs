4015473c8879cdc401d0a3c5b6b766c04bcc51ce
robot <rahaupt0@gmail.com>
2022-07-11 12:00:46 -0400

    added handling of multiple search matches

--- Midterm1/Program.cs
+++ Midterm1/Program.cs
@@ -1,45 +1,53 @@
 ﻿using Midterm1;
 using System.Globalization;
+//File for importing the library
 string filePath = "../../../LibraryBackUp.txt";
 
-//List<Book> AllBooks1 = new List<Book>()
-//{
-//    new Book("Ready Player One", "Ernest Cline", "On Shelf"),
-//    new Book("1984", "George Orwell", "On Shelf"),
-//    new Book("Starship Troopers", "Robert A. Heinlein", "On Shelf"),
-//    new Book("2001: A Space Odyssey", "Arthur C. Clarke", "On Shelf"),
-//    new Book("Twilight", "Stephenie Meyer", "On Shelf"),
-//    new Book("The Hunger Games", "Suzanne Collins", "On Shelf"),
-//    new Book("Astrophysics for People in a Hurry", "Neil deGrasse Tyson", "On Shelf"),
-//    new Book("Scott Pilgrim Vol. 1-6", "Bryan Lee O'Malley", "On Shelf"),
-//    new Book("One Piece Vol. 1-102", "Eiichiro Oda", "On Shelf"),
-//    new Book("The Hobbit", "J.R.R. Tolkien", "On Shelf"),
-//    new Book("Ender's Game", "Orson Scott Card", "On Shelf"),
-//    new Book("The Great Gatsby", "F. Scott Fitzgerald", "On Shelf"),
-//};
+//Initialize list of books in library
 List<Book> AllBooks1 = new List<Book>();
-List<Book> checkedOutBooks1 = new List<Book>();
 
 StreamReader reader = new StreamReader(filePath);
-
-while (true)
+//read in the library.
+if (File.Exists(filePath))
 {
-    string line = reader.ReadLine();
-    if (line == null)//it pulled out current student and found nothing
+    while (true)
     {
-        break;
+        string line = reader.ReadLine();
+        if (line == null)
+        {
+            break;
+        }
+        else
+        {
+            //take line and turn into book
+            string[] AllBooksArray = line.Split(',');
+            Book newBook = new Book(AllBooksArray[0], AllBooksArray[1], AllBooksArray[2]);
+            AllBooks1.Add(newBook);
+        }
     }
-    else//found a student
-    {
-        //Console.WriteLine(line); //debugging line
-        //take line and turn into student
-        string[] AllBooksArray = line.Split(',');
-        Book newBook = new Book(AllBooksArray[0], AllBooksArray[1], AllBooksArray[2]);
-        AllBooks1.Add(newBook);
-    }
+    reader.Close();
 }
-reader.Close();
+else
+{
+    //Initial library for testing
+    AllBooks1 = new List<Book>()
+    {
+    new Book("Ready Player One", "Ernest Cline", "On Shelf"),
+    new Book("1984", "George Orwell", "On Shelf"),
+    new Book("Starship Troopers", "Robert A. Heinlein", "On Shelf"),
+    new Book("2001: A Space Odyssey", "Arthur C. Clarke", "On Shelf"),
+    new Book("Twilight", "Stephenie Meyer", "On Shelf"),
+    new Book("The Hunger Games", "Suzanne Collins", "On Shelf"),
+    new Book("Astrophysics for People in a Hurry", "Neil deGrasse Tyson", "On Shelf"),
+    new Book("Scott Pilgrim Vol. 1-6", "Bryan Lee O'Malley", "On Shelf"),
+    new Book("One Piece Vol. 1-102", "Eiichiro Oda", "On Shelf"),
+    new Book("The Hobbit", "J.R.R. Tolkien", "On Shelf"),
+    new Book("Ender's Game", "Orson Scott Card", "On Shelf"),
+    new Book("The Great Gatsby", "F. Scott Fitzgerald", "On Shelf"),
+    };
+}
 
+//MAIN PROGRAM
 bool library = true;
 while (library)
 {
@@ -72,72 +80,122 @@ writer.Close();
 
 
 //Methods
+//Checks to see if search term is unique, if so, checks out single book. Else, loops with request for better search
 static void CheckOutBook(ref List<Book> AllBooks, ref bool bookInStock)
 {
-    AllBooks.ForEach(b => Console.WriteLine(String.Format("{0,-40} {1,-25} {2,-10}", b.Title, b.Author, b.Status)));
-    Console.WriteLine("\nWhat book would you like to check out?\n");
-    string choice = "";
-    choice = Console.ReadLine().Trim().ToLower();
-
-
-    //Loop over all books currently checked into the library 
-    for (int i = 0; i < AllBooks.Count; i++)
+    while (bookInStock == false)
     {
-        if ((AllBooks[i].Title.ToLower().Contains(choice) || AllBooks[i].Author.ToLower().Contains(choice)) && AllBooks[i].Status == "On Shelf")
-        {
+        //Displays Book List
+        AllBooks.ForEach(b => Console.WriteLine(String.Format("{0,-40} {1,-25} {2,-10}", b.Title, b.Author, b.Status)));
 
-            Console.WriteLine($"You checked out {AllBooks[i].Title} by {AllBooks[i].Author}.");
-            Book notAvailableBook = new Book(AllBooks[i].Title, AllBooks[i].Author, "Checked Out", DateTime.Now.AddDays(14));
-            AllBooks[i].Status = "Checked Out";
-            Console.WriteLine($"Please return this book by {notAvailableBook.ReturnDate}");
-            bookInStock = true;
-            break;
-        }
-        else if ((AllBooks[i].Title.ToLower().Contains(choice) || AllBooks[i].Author.ToLower().Contains(choice)) && AllBooks[i].Status == "Checked Out")
+        //Prompts for search term
+        Console.WriteLine("\nWhat book would you like to check out?\n");
+        string choice = "";
+        choice = Console.ReadLine().Trim().ToLower();
+
+        //check if multiples that fit the "Contains" method
+        List<Book> multipleBooks = checkForMultiples(ref AllBooks, choice);
+        if (multipleBooks.Count() <= 1)
         {
-            Console.WriteLine($"This book is currently checked out.");
-            bookInStock = true;
-            break;
+            for (int i = 0; i < AllBooks.Count; i++)
+            {
+                if ((AllBooks[i].Title.ToLower().Contains(choice) || AllBooks[i].Author.ToLower().Contains(choice)) && AllBooks[i].Status == "On Shelf")
+                {
+                    //If on shelf, gives return date
+                    Console.WriteLine($"You checked out {AllBooks[i].Title} by {AllBooks[i].Author}.");
+                    Book notAvailableBook = new Book(AllBooks[i].Title, AllBooks[i].Author, "Checked Out", DateTime.Now.AddDays(14));
+                    AllBooks[i].Status = "Checked Out";
+                    Console.WriteLine($"Please return this book by {notAvailableBook.ReturnDate}\n");
+                    bookInStock = true;
+                    break;
+                }
+                else if ((AllBooks[i].Title.ToLower().Contains(choice) || AllBooks[i].Author.ToLower().Contains(choice)) && AllBooks[i].Status == "Checked Out")
+                {
+                    //If checked out, tells user
+                    Console.WriteLine($"\nThis book is currently checked out.\n");
+                    bookInStock = true;
+                    break;
+                }
+                else
+                {
+                    bookInStock = false;
+                }
+            }
+            if (bookInStock == false)
+            {
+                //If not in library, tells user
+                Console.WriteLine("\nWe do not have that book.\n");
+                bookInStock = true;
+            }
         }
         else
         {
-            bookInStock = false;
+            //Gives list of books that match search term. Loops to ask again.
+            Console.WriteLine("\nThe books that matched your search are: ");
+            multipleBooks.ForEach(x => Console.WriteLine($"{x.Title}"));
+            Console.WriteLine("Please refine your search.\n");
         }
     }
-
-    if (bookInStock == false)
-    {
-        Console.WriteLine("We do not have that book.\n");
-        bookInStock = true;
-    }
-
 }
 
 static void ReturnBook(ref List<Book> AllBooks, ref bool bookInStock)
 {
-    AllBooks.ForEach(b => Console.WriteLine(String.Format("{0,-40} {1,-25} {2,-10}", b.Title, b.Author, b.Status)));
-    Console.WriteLine("\nWhat book would you like to return?\n");
-    string choice = "";
-    choice = Console.ReadLine();
-    for (int i = 0; i < AllBooks.Count; i++)
+    while (bookInStock == false)
     {
-        if ((AllBooks[i].Title.ToLower().Contains(choice) || AllBooks[i].Author.ToLower().Contains(choice)) && AllBooks[i].Status == "Checked Out")
+        AllBooks.ForEach(b => Console.WriteLine(String.Format("{0,-40} {1,-25} {2,-10}", b.Title, b.Author, b.Status)));
+        Console.WriteLine("\nWhat book would you like to return?\n");
+        string choice = "";
+        choice = Console.ReadLine();
+        //check if multiples that fit the "Contains" method
+        List<Book> multipleBooks = checkForMultiples(ref AllBooks, choice);
+        if (multipleBooks.Count() <= 1)
         {
-            Console.WriteLine($"You returned {AllBooks[i].Title} by {AllBooks[i].Author}.");
-            //Book AvailableBook = new Book(checkedOutBooks[i].Title, checkedOutBooks[i].Author, "On Shelf");
-            List<Book> returnedBook = AllBooks.Where(x => x.Title.ToLower().Contains(choice) || x.Author.ToLower().Contains(choice)).ToList();
-            returnedBook.ForEach(y => y.Status = "On Shelf");
-            bookInStock = true;
-            break;
+            for (int i = 0; i < AllBooks.Count; i++)
+            {
+                if ((AllBooks[i].Title.ToLower().Contains(choice) || AllBooks[i].Author.ToLower().Contains(choice)) && AllBooks[i].Status == "Checked Out")
+                {
+                    Console.WriteLine($"\nYou returned {AllBooks[i].Title} by {AllBooks[i].Author}.");
+                    //Book AvailableBook = new Book(checkedOutBooks[i].Title, checkedOutBooks[i].Author, "On Shelf");
+                    List<Book> returnedBook = AllBooks.Where(x => x.Title.ToLower().Contains(choice) || x.Author.ToLower().Contains(choice)).ToList();
+                    returnedBook.ForEach(y => y.Status = "On Shelf");
+                    bookInStock = true;
+                    break;
+                }
+                else if ((AllBooks[i].Title.ToLower().Contains(choice) || AllBooks[i].Author.ToLower().Contains(choice)) && AllBooks[i].Status == "Checked Out")
+                {
+                    //If checked out, tells user
+                    Console.WriteLine($"\nThis book is currently checked out.\n");
+                    bookInStock = true;
+                    break;
+                }
+                else
+                {
+                    bookInStock = false;
+                }
+            }
+            if (bookInStock == false)
+            {
+                Console.WriteLine("\nThat is not our book.\n");
+            }
         }
         else
         {
-            bookInStock = false;
+            Console.WriteLine("\nThe books that matched your search are: ");
+            multipleBooks.ForEach(x => Console.WriteLine($"{x.Title}"));
+            Console.WriteLine("Please refine your search.\n");
         }
     }
-    if (bookInStock == false)
-    {
-        Console.WriteLine("That is not our book.\n");
-    }
+}
 
+static List<Book> checkForMultiples(ref List<Book> AllBooks, string choice)
+{
+    List<Book> toReturn = new List<Book>();
+    for (int i = 0; i < AllBooks.Count; i++)
+    {
+        if ((AllBooks[i].Title.ToLower().Contains(choice) || AllBooks[i].Author.ToLower().Contains(choice)))
+        {
+            toReturn.Add(AllBooks[i]);
+        }
+    }
+    return toReturn;
 }
